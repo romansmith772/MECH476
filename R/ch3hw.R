e285724d6b9a268908a292866237de19f3d60d43
romansmith772 <104648988+romansmith772@users.noreply.github.com>
2024-09-04 12:51:20 -0600

    Update ch3hw.R

--- R/ch3hw.R
+++ R/ch3hw.R
@@ -11,4 +11,22 @@ str(cleaned_data)
 head(cleaned_data)
 missing_data <- sum(is.na(cleaned_data$ozone_ppm))
 
+top_ten_ozone <- cleaned_data %>%
+  slice_max(order_by=ozone_ppm, n=10)
 
+low_ten_ozone <- cleaned_data %>%
+  slice_min(order_by = ozone_ppm, n=10)
+#question 3
+high_day <- top_ten_ozone %>%
+  slice_max(order_by = ozone_ppm, n=1) %>%
+  pull(datetime)
+low_day <- top_ten_ozone %>%
+  slice_min(order_by = ozone_ppm, n=1) %>%
+  pull(datetime)
+#question 4
+tempc<- #temp in Celsius
+mol_weight_o3 <- 47.998 #grams/mol
+c_to_k <-273.1+tempc #Celsius to kelvin
+pressure_in_ftc <- 637 #atmostpheric pressure in ft collins mmHg
+r <- 22.4136 #universial gas constant
+ozone_ugm3 <- 
